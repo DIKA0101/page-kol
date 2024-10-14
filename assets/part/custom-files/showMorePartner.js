913a5a40893e8d52e0d5ab77d18bd6fa40d9d296
Yoga Pangestu <yogapangestu16@gmail.com>
2024-10-14 13:32:58 +0700

    14-10-2024

--- assets/part/custom-files/showMorePartner.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('showMorePartner').addEventListener('click', function () {
-    var hiddenImages = document.querySelectorAll('#hiddenImages');
-    hiddenImages.forEach(function (image) {
-      image.classList.toggle('d-none');
-    });
-    this.textContent = this.textContent === 'Show More' ? 'Show Less' : 'Show More';
-  });
